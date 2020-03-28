2ed4e00794b78982f3c87bd8f91423aa4f4508c6
Viktor Shatunov <tori690@yandex.ru>
2020-03-28 07:30:37 +0300

    Установлены минимальные размеры основной формы.

--- AntiProsrok/MainForm.Designer.cs
+++ AntiProsrok/MainForm.Designer.cs
@@ -804,6 +804,7 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.mainMenu;
+            this.MinimumSize = new System.Drawing.Size(850, 460);
             this.Name = "MainForm";
             this.Text = "АнтиПросрочка";
             this.mainMenu.ResumeLayout(false);
